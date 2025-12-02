5f787ee051a32e022b88493d005fc8daeb056a10
emmanetan <emmannate283@gmail.com>
2025-12-03 07:07:36 +0800

    Cleaning

--- CalapanCarRentalMVC/Migrations/AddLocationTrackingColumns.sql
+++
@@ -1,6 +0,0 @@
--- Add LocationTracking columns to Users table
-ALTER TABLE Users 
-ADD COLUMN LocationTrackingEnabled TINYINT(1) NOT NULL DEFAULT 0;
-
-ALTER TABLE Users 
-ADD COLUMN LocationTrackingEnabledDate DATETIME(6) NULL;
